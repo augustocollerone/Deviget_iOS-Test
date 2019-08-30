d9f2d9fb02ad048d0f051090774969101dfaf879
Augusto Collerone <collerone.augusto@gmail.com>
2019-08-30 00:41:12 -0300

    EntriesTableViewController & EntryTableViewCell created

---
+++ Deviget_iOS-Test/Deviget_iOS-Test/EntryTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  EntryTableViewCell.swift
+//  Deviget_iOS-Test
+//
+//  Created by Augusto Collerone on 30/08/2019.
+//  Copyright © 2019 Augusto Collerone. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        render()
+    }
+    
+    func render() {
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
